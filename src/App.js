7feddd2553dac71feb477355dc0c81bdbd5a434a
ck <ck@cks-macbook-pro.lan>
2022-02-22 09:22:52 -0800

    useState branch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
+const request = `https://pokeapi.co/api/v2/pokemon?limit=10`;
+const badRequest = `https://pokeapi.co/api/v10/pokemon?limit=10`;
+
 function App() {
+  const [isLoading, setLoading] = useState(false); 
+  const [data, setData ] = useState(null);
+  const [error, setError] = useState(null); 
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(request)
+      .then(response => response.json())
+      .then(data => {
+        setTimeout(() => {
+          setData(data);
+          setLoading(false);
+        }, 5000)
+          
+        return null
+      })
+      .catch(err => setError(err.toString()))
+  }, [])
+if(data){
+  console.log(data.results)
+}
+if(error){
+  console.log(error);
+}
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            {
+        isLoading && 
+        <div>
+          <h1>loading.....</h1>
+        </div>
+      }
+      {
+      error &&
+        <div>
+          <h1>whoopsie!: that was a bad request</h1>
+          <p>{error}</p>
+        </div>
+      }
+      {
+        data &&
+        // data.results.length > 0 &&
+        <div>
+          <h1>top 10 poke</h1>
+            {
+              data.results.map((item, idx) => <div key={idx}>{item.name}</div>)
+            }
+        </div>
+      }
     </div>
   );
 }
